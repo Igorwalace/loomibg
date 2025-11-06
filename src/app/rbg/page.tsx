ab7ca04648fc8adaf9e4150709bff028309abc3c
igorwalace <igorwallacy3@gmail.com>
2025-11-06 18:36:52 -0300

    p-10

--- src/app/rbg/page.tsx
+++ src/app/rbg/page.tsx
@@ -9,11 +9,11 @@ function RBG() {
             <main className='' >
                 <Header />
                 <div className='' >
-                    <div className='text-center p-5 bg-muted rounded-2xl' >
+                    <div className='text-center p-5 bg-muted rounded-2xl pt-10' >
                         <h1 className='mb-4 text-balance text-4xl font-bold text-foreground md:text-5xl' >Removedor de Fundo</h1>
                         <h2 className='text-pretty text-lg leading-relaxed text-muted-foreground' >Apague fundos de diferentes imagens de sua escolha gratuitamente.</h2>
                     </div>
-                    <div className='p-- flex justify-center items-center bg-muted rounded-2xl' >
+                    <div className='p-- flex justify-center items-center bg-muted rounded-2xl pb-10' >
                         {/* brincadeira começa aqui */}
                         <RemoveBG />
                     </div>
